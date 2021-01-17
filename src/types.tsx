5d1b29bd683aef891d9ea34625378ddaec47325f
BretzelLudique <clement.zhang2@gmail.com>
2021-01-17 15:51:13 +0100

    TS navigation

--- src/types.tsx
+++ src/types.tsx
@@ -3,5 +3,14 @@ import { StackNavigationProp } from '@react-navigation/stack';
 
 export type RootStackParamList = {
 	MapHomeScreen: undefined, // undefined because you aren't passing any params to the home screen
-	Profile: { name: string }; 
+	MetroStop: { stopName: string }; 
+};
+
+export type MetroStopScreenNavigationProp = StackNavigationProp<
+    RootStackParamList,
+    'MapHomeScreen'
+>;
+
+export type Props = {
+    navigation: MetroStopScreenNavigationProp;
 };
