51ad752179a2c10d882fd2f2a0478a4654f7fe06
BretzelLudique <clement.zhang2@gmail.com>
2021-01-20 22:19:01 +0100

    better format for coordinate

--- tiny_pyscripts/minilib.py
+++ tiny_pyscripts/minilib.py
@@ -11,12 +11,3 @@ def naive_wd():
 def del_file(filename):
     if path.exists(filename):
         remove(filename)
-
-def is_duplicate(candidate, data):
-    """Returns the index of the occurrence of candidate in data. If no duplicate, returns -1"""
-    i = 0
-    for arret in data["stops"]:
-        if candidate['properties']['nom_iv'] == arret['name']:
-            return i
-        i += 1
-    return -1
--- tiny_pyscripts/to_thinjson.py
+++ tiny_pyscripts/to_thinjson.py
@@ -11,14 +11,25 @@ lib.del_file(dest_filename)
 data = {}
 data['stops'] = []
 
+def find_duplicate_index(candidate, data):
+    """Returns the index of the occurrence of candidate in data. If no duplicate, returns -1"""
+    i = 0
+    for arret in data["stops"]:
+        if candidate['properties']['nom_gare'] == arret['name']:
+            return i
+        i += 1
+    return -1
 
 def add_stop(arret, data):
-    duplicate_index = lib.is_duplicate(arret, data)
+    duplicate_index = find_duplicate_index(arret, data)
 
     if duplicate_index == -1:  # == no duplicate
         data['stops'].append({
-            'name': arret['properties']['nom_iv'],
-            'coordinates': arret['geometry']['coordinates'],
+            'name': arret['properties']['nom_gare'],
+            'coordinate': {
+                'latitude': arret['geometry']['coordinates'][1],
+                'longitude': arret['geometry']['coordinates'][0]
+            },
             'line': arret['properties']['indice_lig']
         })
     else:
