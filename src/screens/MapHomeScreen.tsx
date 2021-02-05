84f6acaa62de0f80577e07742e34835f5b2252b3
BretzelLudique <clement.zhang2@gmail.com>
2021-02-05 01:19:54 +0100

    better search

--- src/screens/MapHomeScreen.tsx
+++ src/screens/MapHomeScreen.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { StyleSheet, View, Image } from 'react-native';
+import { StyleSheet, View, Image, ScrollView, Alert } from 'react-native';
 import { RouteNav, Stop } from '../types';
 import { LinePolylines } from '../components/map/LinePolylines'
-import { Searchbar, List } from 'react-native-paper';
+import { Button, Modal, Portal, Searchbar, List } from 'react-native-paper';
 import MapView, { Marker, Region, MapStyleElement } from 'react-native-maps';
 
 export const MapHomeScreen = ({ navigation }: RouteNav): JSX.Element => {
@@ -13,9 +13,11 @@ export const MapHomeScreen = ({ navigation }: RouteNav): JSX.Element => {
         longitudeDelta: 0.13,
     }
     const stopsArray: Array<Stop> = require("../../assets/map/marker_locs.json");
+    const len: number = stopsArray.length;
     const RetroStyle: Array<MapStyleElement> = require("../../assets/map/retroMapStyle.json");
     const [searchQuery, setSearchQuery] = React.useState('');
     const MarkerRefsArray = React.useRef(new Array);
+    const mapRef = React.useRef(null);
 
     function createMarker(stop: Stop, index: number): JSX.Element {
         return (
@@ -42,46 +44,56 @@ export const MapHomeScreen = ({ navigation }: RouteNav): JSX.Element => {
         )
     }
 
-    const DisplaySearchResult = (stopsArray: Stop[]): Array<JSX.Element | void> => {
-        function displayOneMatchedStop(stop: Stop, index: number): JSX.Element | void {
+    const DisplaySearchResult = (): JSX.Element | null => {
+        function displayOneMatchedStop(stop: Stop, index: number): JSX.Element {
             return (
                 <List.Item
+                    style={styles.stopSearchResult}
                     key={index}
                     title={stop.name}
-                    onPress={() => MarkerRefsArray.current[index].showCallout()}
+                    onPress={() => {
+
+                        MarkerRefsArray.current[index].showCallout();
+                    }}
                     description={stop.line}
                 />
             );
         }
 
-        function matchesQuery(stop: Stop): Boolean {
-            let tmpRegex: string = '';
-            for (let char of searchQuery.toUpperCase()) {
-                tmpRegex += char;
+        const searchResult = [];
+
+        for (let index = 0; index < len; index++) {
+            if (matchedStops[index]) {
+                searchResult.push(displayOneMatchedStop(stopsArray[index], index))
             }
-            if (searchQuery != '') {
-                let regex = new RegExp(tmpRegex);
-                return (regex.test(stop.name))
-            }
-            return (false);
         }
 
-        const displaySearchResult = [];
-        for (let index in stopsArray) {
-            const stop = stopsArray[index];
-            if (matchesQuery(stop)) {
-                displaySearchResult.push(displayOneMatchedStop(stop, parseInt(index)));
-            }
-            if (displaySearchResult.length > 4) {
-                break;
-            }
-        }
-        return displaySearchResult;
+        return (
+            <ScrollView style={styles.searchResultsContainer}>
+                {searchResult}
+            </ScrollView>
+        )
     }
+
+    function matchesQuery(stop: Stop, query: string): Boolean {
+        let tmpRegex: string = '';
+        for (let char of query.toUpperCase()) {
+            tmpRegex += char;
+        }
+        if (query.length > 1) {
+            let regex = new RegExp(tmpRegex);
+            return (regex.test(stop.name))
+        }
+        return (false);
+    }
+
+    const [matchedStops, setMatchedStops] = React.useState(new Array(len).fill(false));
+    const [searching, setSearching] = React.useState(false);
 
     return (
         <View style={styles.container}>
             <MapView
+                ref={mapRef}
                 style={styles.map}
                 initialRegion={ParisRegion}
                 loadingEnabled={true}
@@ -90,14 +102,33 @@ export const MapHomeScreen = ({ navigation }: RouteNav): JSX.Element => {
                 {stopsArray.map(createMarker)}
                 {LinePolylines()}
             </MapView>
-            <View>
-                {DisplaySearchResult(stopsArray)}
-                <Searchbar
-                    placeholder="Recherche par nom"
-                    onChangeText={(q: string) => setSearchQuery(q)}
-                    value={searchQuery}
-                />
-            </View>
+            <Portal>
+                <Modal
+                    visible={searching}
+                    onDismiss={() => setSearching(false)}
+                    contentContainerStyle={[styles.searchContainer, { height: 210 }]}
+                >
+
+                    <DisplaySearchResult />
+
+                    <Searchbar
+                        style={styles.searchButton}
+                        placeholder="Recherche par nom"
+                        onChangeText={(q: string) => {
+                            setSearchQuery(q);
+                            setMatchedStops(
+                                matchedStops.map((matched: Boolean, index: number) =>
+                                    matchesQuery(stopsArray[index], q)
+                                ));
+                        }}
+                        value={searchQuery}
+                    />
+
+                </Modal>
+            </Portal>
+            <Button style={styles.searchButton} onPress={() => setSearching(true)}>
+                Recherche
+      </Button>
         </View>
     );
 }
@@ -108,5 +139,31 @@ const styles = StyleSheet.create({
     },
     map: {
         flex: 1,
-    }
+    },
+    test: {
+        height: 210
+    },
+    searchContainer: {
+        //alignSelf: 'baseline',
+        //flex: 1,
+        height: 210,
+        width: '100%',
+        position: 'absolute',
+        bottom: 0,
+        backgroundColor: 'white',
+    },
+    searchResultsContainer: {
+        //backgroundColor: 'white',
+    },
+    stopSearchResult: {
+        height: 40
+    },
+    searchButton: {
+        //flex: 1,
+        height: 40,
+        position: 'absolute',
+        bottom: 0,
+        width: '100%',
+        backgroundColor: 'white',
+    },
 });
